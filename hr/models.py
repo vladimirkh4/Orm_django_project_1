558caef83592ac1f5821da32f33fefbd28b3997f
vladimir4512 <vladim2972@gmail.com>
2025-03-19 18:03:55 +0300

    add class Compensation with ManyToMany Employee

--- hr/models.py
+++ hr/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 
+class Compensation(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
 class Contact(models.Model):
     phone = models.CharField(max_length=50, unique=True)
     address = models.CharField(max_length=50)
@@ -22,6 +29,7 @@ class Employee(models.Model):
     last_name = models.CharField(max_length=100)
     contact = models.OneToOneField(Contact, on_delete=models.CASCADE, null=True)
     department = models.ForeignKey(Department, on_delete=models.CASCADE, default=None)
+    compensations = models.ManyToManyField(Compensation)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
