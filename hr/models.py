42c00a96ebc9555e10a87a651e86034706030d85
vladimir4512 <vladim2972@gmail.com>
2025-03-19 17:20:56 +0300

    add class Contact with OneToOne Employee

--- hr/models.py
+++ hr/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
 
+class Contact(models.Model):
+    phone = models.CharField(max_length=50, unique=True)
+    address = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.phone
+
+
 class Employee(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    contact = models.OneToOneField(Contact, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
-        return f'{self.first_name}, {self.last_name}'
+        return f'{self.first_name} {self.last_name}'
