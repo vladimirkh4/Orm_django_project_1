a400416c0d0a6a0120aea400b16e3ce8a8113dd6
vladimir4512 <vladim2972@gmail.com>
2025-03-19 17:58:17 +0300

    add class Department with OneToMany Employee

--- hr/models.py
+++ hr/models.py
@@ -9,10 +9,19 @@ class Contact(models.Model):
         return self.phone
 
 
+class Department(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Employee(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     contact = models.OneToOneField(Contact, on_delete=models.CASCADE, null=True)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, default=None)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
