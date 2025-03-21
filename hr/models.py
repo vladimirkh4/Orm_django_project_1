26e3358fa19a437abafe8da4e5ed0b9904dd95c3
Vladimir Bashlykov <vlad_29_72@rambler.ru>
2025-03-21 17:18:41 +0300

    add CustomManager

--- hr/models.py
+++ hr/models.py
@@ -25,6 +25,25 @@ class Department(models.Model):
         return self.name
 
 
+class EmployeeQuerySet(models.QuerySet):
+    def get_experience(self):
+        return self.filter(work_experience__gt=3)
+
+    def get_not_experience(self):
+        return self.filter(work_experience__lte=3)
+
+
+class CustomManager(models.Manager):
+    def get_queryset(self):
+        return EmployeeQuerySet(self.model, using=self._db)
+
+    def get_experience(self):
+        return self.get_queryset().get_experience()
+
+    def get_not_experience(self):
+        return self.get_queryset().get_not_experience()
+
+
 class Person(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
@@ -40,6 +59,9 @@ class Employee(Person):
     work_experience = models.SmallIntegerField(default=0)
     department = models.ForeignKey(Department, on_delete=models.CASCADE, default=None)
     compensations = models.ManyToManyField(Compensation)
+
+    objects = models.Manager()
+    persons = CustomManager()
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
