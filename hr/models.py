5f467538718729c27ea73462fccf1c4e5b8562b1
Vladimir Bashlykov <vlad_29_72@rambler.ru>
2025-03-21 16:32:51 +0300

    Add two classes
    abstract class Person and class Trainee

--- hr/models.py
+++ hr/models.py
@@ -25,15 +25,28 @@ class Department(models.Model):
         return self.name
 
 
-class Employee(models.Model):
+class Person(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     age = models.SmallIntegerField(null=True)
     created = models.DateTimeField(default=timezone.now)
-    work_experience = models.SmallIntegerField(default=0)
     contact = models.OneToOneField(Contact, on_delete=models.CASCADE, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class Employee(Person):
+    work_experience = models.SmallIntegerField(default=0)
     department = models.ForeignKey(Department, on_delete=models.CASCADE, default=None)
     compensations = models.ManyToManyField(Compensation)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Trainee(Person):
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, default=None)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
