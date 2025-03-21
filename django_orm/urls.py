e18cd425aa67a626135f4b93c7046da995a4fe41
Vladimir Bashlykov <vlad_29_72@rambler.ru>
2025-03-21 18:39:29 +0300

    fix path 'hr.urls'

--- django_orm/urls.py
+++ django_orm/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('hr/urls')),
+    path('', include('hr.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
 ]
